2489e2ce33f3a660e398f67aa698c6e3b070882d
kakoatao <40969130+kakoatao@users.noreply.github.com>
2018-07-17 16:57:02 -0400

    made progress, but still outline

--- Pulson Code/pulson440_SAR.py
+++ Pulson Code/pulson440_SAR.py
@@ -7,15 +7,22 @@ import sys
 
 
 #Loads the file ---WIP--- SEE:'fileName'
-def unpickle(fileName):
+def unpickle(fileName, legacy=False):
     pickle.load(fileName ,fix_imports=True, encoding="ASCII", errors="strict")
 
+#Handles the arguments given in the console
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='PulsON440 SAR Image former')
+    parser.add_argument('-l --legacy', action='store_true', dest='legacy',
+                        help='Load legacy format of file')
+    parser.add_argument('-f', '--file', dest='file', help='PulsON 440 data file')
+
 #Main function, creates the SAR image
-def main():
-    print(sys.argv[1:])
-    
+def main(args):
+    #Gives arguments
+    #args = parse_args(args) ---WIP--- parse_args() is still broken
     #Loads file
-""" unpickle(arg) """
+    unpickle(arg.file, arg.legacy)#---WIP--- unpickle() is unknown
     
     #Mathematical functions
     
@@ -23,5 +30,5 @@ def main():
 
 #Starts the file's purpose on loading
 if __name__ == "__main__":
-    main()  #Used for testing file
-    #main(sys.argv[1:])  <-Use in final version
+    #main()  #Used for testing file
+    main(sys.argv[1:])  #<-Use in final version
