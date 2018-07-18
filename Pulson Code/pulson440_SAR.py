c5c7247abda51a0a6376902dab2760508d3f8272
kakoatao <40969130+kakoatao@users.noreply.github.com>
2018-07-18 10:44:22 -0400

    unpickle() returns, math functions get outline

--- Pulson Code/pulson440_SAR.py
+++ Pulson Code/pulson440_SAR.py
@@ -4,10 +4,12 @@ import numpy as np
 import argparse
 import sys
 
-"""
-KNOWN BUGS:
-    -Certain files are 'unrecognized arguments'
-"""    
+#Loads a given file
+def unpickle(args):
+    f = open(arg, 'rb')
+    data = pickle.load(arg)
+    f.close()
+    return data
 
 #Handles the arguments given in the console
 def parse_args(args):
@@ -22,12 +24,35 @@ def main(args):
     args = parse_args(sys.argv[1:])
     print(sys.argv[1:])
     print(args.file)
-#Loads file - Replaced 'unpickle()'
-    f = open(args.file, 'rb')
-    data = pickle.load(args.file)
-    f.close()
+#Loads file
+    unpickle(args.file)
 #Mathematical functions
-    
+    """
+    Steps & proposed solutions
+        1: Locate the reference point
+            rangeBins = 'number of horizontal cells' - 1
+            
+            x-axis = data[0] 'row 1'
+            y-axis = data[1] 'row 2'
+            z-axis = data[2] 'row 3'
+            
+            xMax = max(x-axis)
+            xMin = min(x-axis)
+            yMax = max(y-axis)
+            yMin = min(y-axis)
+            zMax = max(z-axis)
+            zMin = min(z-axis)
+            
+            xCenter = Math.round(xMax-((xMax-xMin)/2))
+            yCenter = Math.round(yMax-((yMax-yMin)/2))
+            zCenter = Math.round(zMax-((zMax-zMin)/2))
+        2: Determine the image size/location/resolution (Square image)
+        3: Compensate for time & position
+            for n:
+                time?[n] = 2(R[reference]-R[n])/SPEED_OF_LIGHT
+        4: Find the range to each pixel from pulse location
+        5: Fill in the image pixel by pixel using the average signal at that range
+    """
 #Plots the processed data
 
 #Starts the file's purpose on loading
